0abb2c2da30e1b08ee2bf7ab5f298dc36999aeb1
Christopher Suh <ChrisSuh@ChristophersMacbookPro.local>
2017-02-16 13:51:35 -0500

    working highlighter

--- chrome/highlighter.js
+++ chrome/highlighter.js
@@ -13,11 +13,61 @@
 // make an array that stores what the user clicked on and 
 // when the item is in the array 
 
+// when you shift + click select all the elements of the same class 
+
+
 var clickedElements = new Array();
 
-$(document).click(function(){
-    console.log("testDiv".id);
+document.addEventListener("click",function(e){
+    // console.log($(e.target).getPath());
+    if (clickedElements.includes(e.target.id)){
+        // console.log(e.target.id);
+        index = clickedElements.indexOf(e.target.id);
+        // console.log(index);
+        clickedElements.splice(index, 1);
+        // console.log(clickedElements);
+        $(e.target).css("background-color", "");
+        // console.log(clickedElements);
+
+    }
+    else{
+        clickedElements.push(e.target.id);
+        $(e.target).css("background-color", "ffff00");
+        // console.log(clickedElements);
+    } 
+})
+
+
+jQuery.fn.extend({
+    getPath: function () {
+        var path, node = this;
+        while (node.length) {
+            var realNode = node[0], name = realNode.localName;
+            if (!name) break;
+            name = name.toLowerCase();
+
+            var parent = node.parent();
+
+            var sameTagSiblings = parent.children(name);
+            if (sameTagSiblings.length > 1) { 
+                allSiblings = parent.children();
+                var index = allSiblings.index(realNode) + 1;
+                if (index > 1) {
+                    name += ':nth-child(' + index + ')';
+                }
+            }
+
+            path = name + (path ? '>' + path : '');
+            node = parent;
+        }
+
+        return path;
+    }
 });
+
+// $(document).click(function(){
+//     console.log("testDiv".id);
+// });
 
 // $(document).ready(function(){
 //     var id = $("#testDiv").attr("id");
